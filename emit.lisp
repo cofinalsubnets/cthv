5843144b9dcebf059bb16daf2ba132bcf1059fed
gwentacle <gwentacle@gmail.com>
2012-07-12 08:06:54 -0400

    add generate macro

--- emit.lisp
+++ emit.lisp
@@ -1,11 +1,17 @@
 (in-package :csv)
 
-(export '(dump-csv))
+(export '(dump-csv
+          generate
+          row))
 
-(defun dump-csv (struct &key (rs *rs*) (fs *fs*) (quote *quote*))
+(defun dump-csv (struct &key (rs *rs*) (fs *fs*) (quote *quote*) (to-file nil))
   (flet ((join (lst j) (format nil (format nil "~~{~~A~~^~A~~}" j) lst)))
-    (with-csv-parameters (:rs rs :fs fs :quote quote)
-      (join (mapcar (lambda (n) (join (mapcar #'csv-escape n) *fs*)) struct) *rs*))))
+    (let ((dump (with-csv-parameters (:rs rs :fs fs :quote quote)
+                  (join (mapcar (lambda (n) (join (mapcar #'csv-escape n) *fs*)) struct) *rs*))))
+      (if to-file
+        (with-open-file (f to-file :direction :output)
+          (princ dump f)))
+      dump)))
 
 (defun csv-escape (str)
   "Quote a string as needed for CSV output."
@@ -22,3 +28,16 @@
               (member (car (last chars)) +whitespace+))
         (format nil "~A~A~A" *quote* str *quote*)
         str))))
+
+(defmacro generate ((&key (rs *rs*) (fs *fs*) (quote *quote*) (to-file nil)) &body body)
+  (let ((ref (gensym)))
+    `(let ((,ref nil))
+       (flet ((row (l)
+                (setf ,ref (append ,ref (list (mapcar (lambda (n)
+                                                        (if (stringp n)
+                                                          n
+                                                          (format nil "~A" n)))
+                                                      l))))))
+         ,@body
+         (dump-csv ,ref :rs ,rs :fs ,fs :quote ,quote :to-file ,to-file)))))
+
